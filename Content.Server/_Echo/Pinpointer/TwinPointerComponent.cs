cf1d93b6796105447147057fb212b962cbc8ddb8
Red Mushie <noreply@redmushie.me>
2024-12-26 20:09:57 +0100

    Add twinpointers

---
+++ Content.Server/_Echo/Pinpointer/TwinPointerComponent.cs
@@ -0,0 +1,14 @@
+﻿using Robust.Shared.GameStates;
+using Robust.Shared.Serialization;
+
+namespace Content.Server._Echo.Pinpointer;
+
+/// <summary>
+/// Displays a sprite on the item that points towards the target component.
+/// </summary>
+[RegisterComponent, NetworkedComponent]
+[AutoGenerateComponentState]
+[Access(typeof(TwinPointerSystem))]
+public sealed partial class TwinPointerComponent : Component
+{
+}
---
+++ Content.Server/_Echo/Pinpointer/TwinPointerSystem.cs
@@ -0,0 +1,23 @@
+﻿using Content.Server.Pinpointer;
+using Content.Shared.Pinpointer;
+
+namespace Content.Server._Echo.Pinpointer;
+
+public sealed class TwinPointerSystem : EntitySystem
+{
+    [Dependency] private readonly SharedPinpointerSystem _pinpointerSystem = default!;
+
+    public override void Initialize()
+    {
+        base.Initialize();
+        SubscribeLocalEvent<TwinPointerComponent, AfterItemsSpawnEvent>(Handler);
+    }
+
+    private void Handler(EntityUid uid, TwinPointerComponent component, AfterItemsSpawnEvent args)
+    {
+        var left = args.EntityUids[0];
+        var right = args.EntityUids[1];
+        _pinpointerSystem.SetTarget(left, right);
+        _pinpointerSystem.SetTarget(right, left);
+    }
+}
